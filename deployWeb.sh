92d683714cb9fb2ae271156494971fc75228952d
RajivKumarVA <39180471+RajivKumarVA@users.noreply.github.com>
2019-12-15 22:21:51 +0530

    updated

--- deployWeb.sh
+++ deployWeb.sh
@@ -1,8 +1,8 @@
-sudo cp  /var/www/html/index1.html /var/www/html/index1.html_bkp
+sudo cp /var/www/html/index.html /var/www/html/index.html_bkp
 if [ $? -eq 0 ]
 then
-    sudo rm /var/www/html/index1.html
-    sudo cp JenkinsProj/index2.html /var/www/html/
+    sudo rm /var/www/html/index.html
+    sudo cp index.html /var/www/html/
 else
     echo "Error occurred in deploying" >> /tmp/log.txt
     exit 1
